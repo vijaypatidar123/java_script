e6f5c445fbcac962c40dc5de7e984deaf5baa800
vijay patidar <vijaypatidar1900@gmail.com>
2025-02-15 15:23:25 +0530

    changes

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,52 @@
+/*
+let score="33ab"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber)); 
+console.log(valueInNumber);
+
+// "33" => 33
+//  "33abc"=> NaN
+// true => 1; false=>0
+let isLoggeedIn=1
+let booleanIsLoggedIn=Boolean(isLoggeedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 =>false
+// "" => false
+// "hitesh" => true
+*/
+ 
+/*
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+*/
+
+// ********Operations*********
+// let value=3
+// let negValue=-value
+// // console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+// let s1="Hello"
+// let s2=" Vijay"
+// console.log(s1+s2)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
